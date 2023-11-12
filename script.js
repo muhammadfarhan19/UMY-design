421fd0aaf8520d898b482877a923a499438f4ed2
Muhammad Farhan Hamidie <hamidie.farhan@gmail.com>
2023-11-13 01:24:42 +0700

    styling:[page]

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ navItem.forEach((el) => {
   el.addEventListener("click", () => {
     navItem.forEach((item) => {
       if (item !== el) {
-        item.classList.remove("color-primary");
-        item.classList.remove("text-light");
+        item.classList.remove("color-secondary");
+        item.classList.remove("text-dark");
       }
     });
-    el.classList.toggle("color-primary");
-    el.classList.toggle("text-light");
+    el.classList.toggle("color-secondary");
+    el.classList.toggle("text-dark");
   });
 });
